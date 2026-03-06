afe21f1d36b330344f5d76dbbb6218cd021b0ee6
juanj9805 <juanjoseb9805@gmail.com>
2026-03-06 18:07:33 -0500

    docs: .env uploaded

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,12 @@
-const express    = require('express');
-const controller = require('../controllers/authController');
+const express = require("express");
+const controller = require("../controllers/authController");
 
 const router = express.Router();
 
 // POST /api/auth/register
-router.post('/register', controller.register);
+router.post("/register", controller.register);
 
 // POST /api/auth/login
-router.post('/login', controller.login);
+router.post("/login", controller.login);
 
 module.exports = router;
